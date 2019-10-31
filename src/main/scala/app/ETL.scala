753036bf587053c79298c71ffdf05cd35b58721c
Lucas Goncalves <lucas.pro@mail.com>
2019-10-31 09:03:16 +0100

    start ETL

--- src/main/scala/app/ETL.scala
+++ src/main/scala/app/ETL.scala
@@ -1,13 +1,48 @@
+package app
+
+import org.apache.spark.sql.{DataFrame, Row, SQLContext, SparkSession}
+import org.apache.spark.sql.functions.udf
 object ETL {
 
-  // See if we need to use string or int
-  def cleanOs(os: String) {
-    val osLower = os.toLowerCase()
+  def cleaningProcess(df: DataFrame): DataFrame = {
+    val appOrSiteCleaned = cleanAppOrSite(df)
+    val oscleaned = cleanOS(appOrSiteCleaned)
+    oscleaned
+  }
 
-    if (osLower.contains("win")) "windows"
-    else if (osLower.contains("ios")) "ios"
-    else if (osLower.contains("android")) "android"
-    else if (osLower.contains("rim")) "rim"
-    else "other"
+//  ==== Cleaning process for appOrSite Column ====
+  def cleanAppOrSite(df: DataFrame): DataFrame = {
+    val transformUDF = udf { appOrSite: String =>
+      if (appOrSite == "app") 1.0
+      else if (appOrSite == "site") 0.0
+      else -1.0
+    }
+    val appOrSiteCleaned = transformUDF(df.col("appOrSite"))
+    val newDataFrame = df.withColumn("appOrSite", appOrSiteCleaned)
+    newDataFrame
+  }
+
+  // ==== Cleaning OS ====
+
+  def cleanOS(df: DataFrame): DataFrame = {
+    val transformUDF = udf { os: String =>
+      if (os.toLowerCase() == "android") 1.0
+      else if (os.toLowerCase() == "ios") 2.0
+      else if (os == "WindowsMobile" || os == "Windows Mobile OS" || os == "WindowsPhone" || os == "Windows Phone OS")
+        3.0
+      else if (os.toLowerCase() == "windows") 4.0
+      else if (os.toLowerCase().contains("bada")) 5.0
+      else if (os.toLowerCase().contains("rim")) 6.0
+      else if (os.toLowerCase().contains("symbian")) 7.0
+      else if (os.toLowerCase().contains("webos")) 8.0
+      else if (os.toLowerCase().contains("blackberry")) 9.0
+      else if (os.toLowerCase() == "other" || os.toLowerCase() == "Unknown")
+        10.0
+      else -1.0
+    }
+
+    val osCleaned = transformUDF(df.col("os"))
+    val newDataFrame = df.withColumn("os", osCleaned)
+    newDataFrame
   }
 }
--- src/main/scala/app/Launcher.scala
+++ src/main/scala/app/Launcher.scala
@@ -1,7 +1,11 @@
+package app
+
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.{DataFrame, Row, SQLContext, SparkSession}
-import org.apache.spark.mllib.stat.{MultivariateStatisticalSummary, Statistics}
+import org.apache.spark.sql.functions.udf
+import app.ETL
+//import org.apache.spark.mllib.stat.{MultivariateStatisticalSummary, Statistics}
 
 object Launcher extends App {
   // Load data here :
@@ -11,11 +15,11 @@ object Launcher extends App {
       .appName("imgp")
       .getOrCreate()
 
-  val rawData: DataFrame = spark.read.json("src/data/data-students.json")
-  rawData.show(10)
-  val summary: MultivariateStatisticalSummary = Statistics(rawData)
-  println(summary.mean)
-  //rawData.select($"npaHeaderData.npaNumber".as("npaNumber"))
-  //val someDF = spark.createDataFrame(spark.sparkContext.parallelize(rawData))
+  // EXtract all datas for learning
+  val rawData: DataFrame = spark.read.json("src/data/small.json")
 
+  // ETL PROCESS
+  val appOrSite = rawData.select("appOrSite")
+  val newDf = ETL.cleaningProcess(rawData)
+  println(newDf.select("os").distinct.show(100))
 }
