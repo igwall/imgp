b7f3097ebb09f47b571413dc4e7c183203f9a236
Yannick Mayeur <yannick.mayeur@gmail.com>
2019-11-10 12:16:11 +0100

    Add command line args and delete prediction folder

--- src/main/scala/app/Launcher.scala
+++ src/main/scala/app/Launcher.scala
@@ -4,23 +4,48 @@ import scala.io.StdIn.readLine
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.{DataFrame, Row, SQLContext, SparkSession}
 import org.apache.spark.sql.functions.udf
+import java.io.File
 //import org.apache.spark.mllib.stat.{MultivariateStatisticalSummary, Statistics}
 
-object Launcher extends App {
+object Launcher {
 
-  // EXtract all datas for learning
-  //val rawData: DataFrame = spark.read.json("src/data/data-students.json")
+  def main(args: Array[String]) {
+    args.toList match {
+      case "predict" :: Nil =>
+        deleteOldPredictions()
+        println("\nPlease enter the path of the JSON file that you want to predict")
+        val filePath: String = readLine().trim()
+        Prediction.predict(filePath, "./savedModel")
+      case "learn" :: Nil =>
+        val spark = SparkSession.builder
+            .config("spark.master", "local")
+            .appName("imgp")
+            .getOrCreate()
+         //EXtract all datas for learning
+        val rawData: DataFrame = spark.read.json("src/data/small.json")
 
-  println("\nPlease enter the path of the JSON file that you want to predict")
-  val filePath: String = readLine().trim()
-  Prediction.predict(filePath, "./savedModel")
-  // ETL PROCESS
-  //val newDf = ETL.cleaningProcess(rawData, true)
-  // ===== Create the model : =====
+         //ETL PROCESS
+        val newDf = ETL.cleaningProcess(rawData, true)
+         //===== Create the model : =====
 
-  // Calc the ratio for each label :
-  //LogisticModel.createModel(newDf)
+         //Calc the ratio for each label :
+        LogisticModel.createModel(newDf)
 
-  //spark.close()
+        spark.close()
+      case _ => println("To launch progam use argument predict or learn")
+    }
+  }
+
+  def deleteOldPredictions(): Unit = {
+    def deleteRecursively(file: File) {
+      if (file.isDirectory) {
+        file.listFiles.foreach(deleteRecursively)
+      }
+      if (file.exists && !file.delete) {
+        throw new Exception(s"Unable to delete ${file.getAbsolutePath}")
+      }
+    }
+    deleteRecursively(new File("predictions"))
+  }
 
 }
